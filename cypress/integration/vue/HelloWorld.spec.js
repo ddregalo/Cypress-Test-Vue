d8732d6a414c70bc354eaaa4bc00b88c1544939d
danielcampos <ddregalo@gmail.com>
2018-08-25 11:19:25 +0100

    Test all links in Essential links section

--- cypress/integration/vue/HelloWorld.spec.js
+++ cypress/integration/vue/HelloWorld.spec.js
@@ -23,9 +23,42 @@ describe('Vue App Welcome Page', () => {
             cy.request('https://vuejs.org').then((response) => {
                 expect(response.status).to.eq(200);
             });
-            cy.request('https://jsonplaceholder.typicode.com/comments').then((response) => {
-                expect(response.status).to.eq(200)
-            })
+        });
+
+        it('Forum links to correct and working url', () => {
+            cy.get('ul>li>a')
+                .eq(1)
+                .should('have.attr', 'href').and('include', 'https://forum.vuejs.org');
+            cy.request('https://forum.vuejs.org').then((response) => {
+                expect(response.status).to.eq(200);
+            });
+        });
+
+        it('Community Chat links to correct and working url', () => {
+            cy.get('ul>li>a')
+                .eq(2)
+                .should('have.attr', 'href').and('include', 'https://chat.vuejs.org');
+            cy.request('https://chat.vuejs.org').then((response) => {
+                expect(response.status).to.eq(200);
+            });
+        });
+
+        it('Vue Twitter links to correct and working url', () => {
+            cy.get('ul>li>a')
+                .eq(3)
+                .should('have.attr', 'href').and('include', 'https://twitter.com/vuejs');
+            cy.request('https://twitter.com/vuejs').then((response) => {
+                expect(response.status).to.eq(200);
+            });
+        });
+
+        it('Docs for This Template links to correct and working url', () => {
+            cy.get('ul>li>a')
+                .eq(4)
+                .should('have.attr', 'href').and('include', 'http://vuejs-templates.github.io/webpack/');
+            cy.request('http://vuejs-templates.github.io/webpack/').then((response) => {
+                expect(response.status).to.eq(200);
+            });
         });
 
     });
