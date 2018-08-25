aa5c59eb153d6afbc21b65bcea92984e8c551d3b
danielcampos <ddregalo@gmail.com>
2018-08-25 01:32:49 +0100

    Test Core Docs link

--- cypress/integration/vue/HelloWorld.spec.js
+++ cypress/integration/vue/HelloWorld.spec.js
@@ -1,4 +1,4 @@
-context('Vue App Welcome Page', () => {
+describe('Vue App Welcome Page', () => {
     
     beforeEach(() => {
       cy.visit('http://localhost:8080')
@@ -8,6 +8,26 @@ context('Vue App Welcome Page', () => {
         cy
             .get('h1')
             .contains('Welcome to Your Vue.js App');
+    });
+
+    describe('Essential Links Section', () => {
+        it('has section title', () => {
+            cy
+                .get('h2')
+                .contains('Essential Links');
+        });
+        it('Core Docs links to correct and working url', () => {
+            cy.get('ul>li>a')
+                .eq(0)
+                .should('have.attr', 'href').and('include', 'https://vuejs.org');
+            cy.request('https://vuejs.org').then((response) => {
+                expect(response.status).to.eq(200);
+            });
+            cy.request('https://jsonplaceholder.typicode.com/comments').then((response) => {
+                expect(response.status).to.eq(200)
+            })
+        });
+
     });
 });
 
