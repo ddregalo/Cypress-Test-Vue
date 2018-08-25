63e52298672cc39255e804d562d3ff4b2f71cc0c
danielcampos <ddregalo@gmail.com>
2018-08-25 11:26:26 +0100

    Update test names + assert link content

--- cypress/integration/vue/HelloWorld.spec.js
+++ cypress/integration/vue/HelloWorld.spec.js
@@ -19,42 +19,47 @@ describe('Vue App Welcome Page', () => {
         it('Core Docs links to correct and working url', () => {
             cy.get('ul>li>a')
                 .eq(0)
+                .should('contain', 'Core Docs')
                 .should('have.attr', 'href').and('include', 'https://vuejs.org');
             cy.request('https://vuejs.org').then((response) => {
                 expect(response.status).to.eq(200);
             });
         });
 
-        it('Forum links to correct and working url', () => {
+        it('Forum link content is correct and links to designated working url', () => {
             cy.get('ul>li>a')
                 .eq(1)
+                .should('contain', 'Forum')
                 .should('have.attr', 'href').and('include', 'https://forum.vuejs.org');
             cy.request('https://forum.vuejs.org').then((response) => {
                 expect(response.status).to.eq(200);
             });
         });
 
-        it('Community Chat links to correct and working url', () => {
+        it('Community Chat link content is correct and links to designated working url', () => {
             cy.get('ul>li>a')
                 .eq(2)
+                .should('contain', 'Community Chat')
                 .should('have.attr', 'href').and('include', 'https://chat.vuejs.org');
             cy.request('https://chat.vuejs.org').then((response) => {
                 expect(response.status).to.eq(200);
             });
         });
 
-        it('Vue Twitter links to correct and working url', () => {
+        it('Vue Twitter link content is correct and links to designated working url', () => {
             cy.get('ul>li>a')
                 .eq(3)
+                .should('contain', 'Twitter')
                 .should('have.attr', 'href').and('include', 'https://twitter.com/vuejs');
             cy.request('https://twitter.com/vuejs').then((response) => {
                 expect(response.status).to.eq(200);
             });
         });
 
-        it('Docs for This Template links to correct and working url', () => {
+        it('Docs for This Template link content is correct and links to designated working url', () => {
             cy.get('ul>li>a')
                 .eq(4)
+                .should('contain', 'Docs for This Template')
                 .should('have.attr', 'href').and('include', 'http://vuejs-templates.github.io/webpack/');
             cy.request('http://vuejs-templates.github.io/webpack/').then((response) => {
                 expect(response.status).to.eq(200);
